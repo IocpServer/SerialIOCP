4d9fbc13c5d84cb9117de705d7826f7acf90d3e6
IocpServer <56920702+IocpServer@users.noreply.github.com>
2019-10-23 20:35:36 +0200

    The Help URL is wrong
    change to http://www.aiku.systems/iocp/arduino/serialiocp/help/

--- SerialIocpClient/src/SerialIocpClient.cpp
+++ SerialIocpClient/src/SerialIocpClient.cpp
@@ -2,6 +2,6 @@
  Name:		SerialIocpClient.cpp
  Created:	29/08/2019 16:42:03
  Author:	Juanra (Aiku Systems)
- Editor:	http://www.aiku.systems/iocp/arduino/serial/help/
+ Editor:	http://www.aiku.systems/iocp/arduino/serialiocp/help/
 */
 
